abc53a6cc94282157405d2e589852e48b3b52b80
jeffrepo <jeffer.leo93@gmail.com>
2025-01-22 12:17:29 -0600

    Equipo de ventas

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "ESN",
+
+    'summary': """ Extras para ESN """,
+
+    'description': """
+        Extras para ESN
+    """,
+
+    'author': "JS",
+    'website': "",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['stock','sale_stock'],
+
+    'data': [
+    ],   
+}
---
+++ models/stock_picking.py
@@ -0,0 +1,11 @@
+from odoo import api, fields, models, tools, _
+
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+
+    team_id = fields.Many2one('crm.team',
+        string="Equipo de ventas",
+        related='sale_id.team_id',
+        store=True, readonly=False)
